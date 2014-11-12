5c5e79e98a219e2816b66a685f56301446167b2c
Doug Simon <doug.simon@oracle.com>
2014-11-12 12:55:21 +0100

    made HotSpotCodeCacheProvider attempt to load and use the hcfdis tool

--- mx/mx_graal.py
+++ mx/mx_graal.py
@@ -1146,7 +1146,8 @@ def _unittest(args, annotations, prefixCp="", blacklist=None, whitelist=None, ve
     if testfile is None:
         (_, testfile) = tempfile.mkstemp(".testclasses", "graal")
         os.close(_)
-    coreCp = mx.classpath(['com.oracle.graal.test'])
+        
+    coreCp = mx.classpath(['com.oracle.graal.test', 'HCFDIS'])
 
     coreArgs = []
     if verbose:
