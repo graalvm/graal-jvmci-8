e6cd5a12267ac0436ceb58bf024877263a8b321a
Gilles Duboscq <duboscq@ssw.jku.at>
2014-11-17 14:58:39 +0100

    When deoptimizing on unwind, revoke biases instead of calling deoptimize_frame before returning deopt continuation

--- src/share/vm/runtime/sharedRuntime.cpp
+++ src/share/vm/runtime/sharedRuntime.cpp
@@ -663,7 +663,9 @@ address SharedRuntime::compute_compiled_exc_handler(nmethod* nm, address ret_pc,
       RegisterMap reg_map(thread);
       frame runtime_frame = thread->last_frame();
       frame caller_frame = runtime_frame.sender(&reg_map);
-      Deoptimization::deoptimize_frame(thread, caller_frame.id(), Deoptimization::Reason_not_compiled_exception_handler);
+      assert(caller_frame.is_compiled_frame() && !caller_frame.is_deoptimized_frame(),"should be called by compiled frame");
+      Deoptimization::revoke_biases_of_monitors(thread, caller_frame, &reg_map);
+
       return SharedRuntime::deopt_blob()->unpack_with_exception_in_tls();
     }
   }
