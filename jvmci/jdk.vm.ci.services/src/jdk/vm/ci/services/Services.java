14716509ff4e777dd0282528e3f4d36b487cdc54
Doug Simon <doug.simon@oracle.com>
2017-04-15 23:53:53 +0200

    changed return type of Services.getSavedProperties (JDK-8177845)

--- jvmci/jdk.vm.ci.services/src/jdk/vm/ci/services/Services.java
+++ jvmci/jdk.vm.ci.services/src/jdk/vm/ci/services/Services.java
@@ -25,6 +25,8 @@ package jdk.vm.ci.services;
 import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.Formatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.ServiceLoader;
 
@@ -45,7 +47,7 @@ public final class Services {
      * Gets the system properties saved when {@link System} is initialized. The caller must not
      * modify the returned value.
      */
-    public static Properties getSavedProperties() {
+    public static Map<String, String> getSavedProperties() {
         SecurityManager sm = System.getSecurityManager();
         if (sm != null) {
             sm.checkPermission(new JVMCIPermission());
@@ -53,7 +55,12 @@ public final class Services {
         try {
             Field savedPropsField = VM.class.getDeclaredField("savedProps");
             savedPropsField.setAccessible(true);
-            return (Properties) savedPropsField.get(null);
+            Properties props = (Properties) savedPropsField.get(null);
+            Map<String, String> res = new HashMap<>(props.size());
+            for (Map.Entry<Object, Object> e : props.entrySet()) {
+                res.put((String) e.getKey(), (String) e.getValue());
+            }
+            return res;
         } catch (Exception e) {
             throw new InternalError(e);
         }
