058e4fce10420e7d28599b5b7ea5736bef1778f0
Doug Simon <doug.simon@oracle.com>
2019-03-04 21:38:53 +0100

    remove JVMCILibArgs and JVMCILibArgsSep flags

--- src/share/vm/jvmci/jvmci_globals.cpp
+++ src/share/vm/jvmci/jvmci_globals.cpp
@@ -179,8 +179,6 @@ bool JVMCIGlobals::check_jvmci_flags_are_consistent() {
   CHECK_NOT_SET(JVMCIPrintProperties,         EnableJVMCI)
   CHECK_NOT_SET(UseJVMCINativeLibrary,        EnableJVMCI)
   CHECK_NOT_SET(JVMCILibPath,                 EnableJVMCI)
-  CHECK_NOT_SET(JVMCILibArgs,                 EnableJVMCI)
-  CHECK_NOT_SET(JVMCILibArgsSep,              EnableJVMCI)
   CHECK_NOT_SET(JVMCILibDumpJNIConfig,        EnableJVMCI)
 
 #ifndef PRODUCT
