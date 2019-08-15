46254fe26f60b34b7d0249b63a4fc349c65cf330
Mohamed EZ-ZARGHILI <mohamed.ez.zarghili@oracle.com>
2019-08-15 13:17:06 +0100

    Update to jvmci-19.3-b02-dev.

--- mx.jvmci/suite.py
+++ mx.jvmci/suite.py
@@ -2,8 +2,8 @@ suite = {
   "mxversion" : "5.215.7",
   "name" : "jvmci",
 
-  "version" : "19.3-b01",
-  "release" : True,
+  "version" : "19.3-b02",
+  "release" : False,
 
   "url" : "http://openjdk.java.net/projects/graal",
   "developer" : {
