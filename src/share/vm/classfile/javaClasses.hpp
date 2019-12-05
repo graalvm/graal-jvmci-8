a51b46d4d87c4cbe7f916f24c39eba21cd7a376c
Tom Rodriguez <tom.rodriguez@oracle.com>
2019-12-05 11:58:07 -0800

    Simplify StackTraceElement::decode

--- src/share/vm/classfile/javaClasses.hpp
+++ src/share/vm/classfile/javaClasses.hpp
@@ -1355,7 +1355,6 @@ class java_lang_StackTraceElement: AllStatic {
   static oop create(methodHandle method, int bci, TRAPS);
 
 #if INCLUDE_JVMCI
-  static void decode(Handle mirror, int method, int version, int bci, int cpref, Symbol*& methodName, Symbol*& fileName, int& lineNumber);
   static void decode(Handle mirror, methodHandle method, int bci, Symbol*& methodName, Symbol*& fileName, int& lineNumber);
 #endif
 
