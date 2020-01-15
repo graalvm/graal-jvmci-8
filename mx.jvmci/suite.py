960f20b52e8214e8afea53838d55538b1a9999e5
Doug Simon <doug.simon@oracle.com>
2020-01-15 15:10:52 +0100

    Start jvmci-20.0-b02 dev cycle

--- mx.jvmci/suite.py
+++ mx.jvmci/suite.py
@@ -2,8 +2,8 @@ suite = {
   "mxversion" : "5.215.7",
   "name" : "jvmci",
 
-  "version" : "20.0-b01",
-  "release" : True,
+  "version" : "20.0-b02",
+  "release" : False,
 
   "url" : "http://openjdk.java.net/projects/graal",
   "developer" : {
