openjdk/jdk

8269934: RunThese24H.java failed with EXCEPTION_ACCESS_VIOLATION in java_lang_Thread::get_thread_status
Reviewed-by: stuefe, dcubed, sspitsyn

src/hotspot/share/services/threadService.cpp
@@ -874,7 +874,10 @@ void ThreadSnapshot::initialize(ThreadsList * t_list, JavaThread* thread) {
   _sleep_ticks = stat->sleep_ticks();
   _sleep_count = stat->sleep_count();
 
-  _thread_status = java_lang_Thread::get_thread_status(threadObj);
+  // If thread is still attaching then threadObj will be NULL.
+  _thread_status = threadObj == NULL ? JavaThreadStatus::NEW
+                                     : java_lang_Thread::get_thread_status(threadObj);
+
   _is_suspended = thread->is_suspended();
   _is_in_native = (thread->thread_state() == _thread_in_native);
 

graalvm/graal-jvmci-8

src/share/vm/services/threadService.cpp
@@ -786,7 +786,10 @@ ThreadSnapshot::ThreadSnapshot(JavaThread* thread) {
   _blocker_object = NULL;
   _blocker_object_owner = NULL;
 
-  _thread_status = java_lang_Thread::get_thread_status(_threadObj);
+  // If thread is still attaching then threadObj will be NULL.
+  _thread_status = _threadObj == NULL ? java_lang_Thread::NEW
+                                      : java_lang_Thread::get_thread_status(_threadObj);
+
   _is_ext_suspended = thread->is_being_ext_suspended();
   _is_in_native = (thread->thread_state() == _thread_in_native);
 